6e909eeadbb693bb0e370e8684b0457fe5860196
Santiago Vargas <santiagovargas921@gmail.com>
2024-01-08 10:23:45 -0500

    merge and strata tools updated

--- berkeley_pes/source/scripts/read_and_split.py
+++ berkeley_pes/source/scripts/read_and_split.py
@@ -1,21 +1,54 @@
+import os 
+from glob import glob
 from berkeley_pes.source.utils.io import write_dict_to_ase_trajectory
 from berkeley_pes.source.utils.parse import parse_json
 
+def merge_folders(folder, out_name): 
+    # merge all files ending with *xyz in the folder and save them as out_name
+    # folder: folder containing the files to merge
+    # out_name: name of the merged file
+    
+    # traverse all files in the folder up to depth 3 
+    files = glob(folder + "/**/*.xyz", recursive=True)
+    # open out_name file 
+    with open(folder + out_name, "w") as outfile:
+        for file in files:
+            #print(file)
+            # open each file and append it to the outfile
+            with open(file) as infile:
+                for line in infile:
+                    outfile.write(line)
 
 def main():
+    #rapter_file = (
+    #    "/home/santiagovargas/dev/berkeley_pes/data/test_rapter.json"
+    #)
+
     rapter_file = (
         "/home/santiagovargas/dev/berkeley_pes/data/20230414_rapter_tracks_initial.json"
     )
     data_rapter = parse_json(rapter_file, mode="normal", verbose=True)
     print("number of frames: {}".format(len(data_rapter)))
-
+    root_out = "../../../data/ase/rapter_full_splits/"
     write_dict_to_ase_trajectory(
         data_rapter,
-        "../../../data/ase/rapter_full/",
+        root_out,
         separate_composition=True,
         separate_spin=True,
         separate_charges=True,
+        train_prop=0.7,
+        val_prop=0.15,
+        test_prop=0.15,
     )
+    
+    val_folder = root_out + "val/"
+    train_folder = root_out + "train/"
+    test_folder = root_out + "test/"
+    
+    merge_folders(val_folder, "val.xyz")
+    merge_folders(train_folder, "train.xyz")
+    merge_folders(test_folder, "test.xyz")
+
 
 
 main()
--- berkeley_pes/source/utils/io.py
+++ berkeley_pes/source/utils/io.py
@@ -593,6 +593,23 @@ def write_ase(f, elements, energy, gradient, xyzs):
                 grad[2],
             )
         )
+def draw_split(train, val, test): 
+    assert train + val + test == 1
+
+    train = int(train * 100)
+    val = int(val * 100)
+    test = int(test * 100)
+    # draw from 0 to 100
+    draw = np.random.randint(0, 100)
+    if draw < train:
+        return "train"
+    elif draw < train + val:
+        return "val"
+    else:
+        return "test"
+    
+
+    
 
 
 def write_dict_to_ase_trajectory(
@@ -601,6 +618,9 @@ def write_dict_to_ase_trajectory(
     separate_charges=False,
     separate_composition=False,
     separate_spin=False,
+    train_prop=0.8,
+    val_prop=0.1,
+    test_prop=0.1,
 ):
     """
     Takes a dictionary organized by trajectories and writes it to an ase file
@@ -610,6 +630,9 @@ def write_dict_to_ase_trajectory(
         separate_charges: whether to separate by charges
         separate_composition: whether to separate by composition
         separate_spin: whether to separate by spin
+        train_prop: proportion of data to use for training
+        val_prop: proportion of data to use for validation
+        test_prop: proportion of data to use for testing
     Returns:
         None
     """
@@ -622,12 +645,26 @@ def write_dict_to_ase_trajectory(
     charge_list = dict_info["charges"]
     charge_list_single = [i[0] for i in charge_list]
     list_charge_unique = np.unique(charge_list_single)
+    list_split_folders = ["/train", "/val", "/test"]
+
+    if test_prop > 0:
+        # make folder 
+        if not os.path.exists(root_out + "test"):
+            os.makedirs(root_out + "test", exist_ok=True)
+    if val_prop > 0:
+        # make folder 
+        if not os.path.exists(root_out + "val"):
+            os.makedirs(root_out + "val", exist_ok=True)
+
+    if not os.path.exists(root_out + "train"):
+        os.makedirs(root_out + "train", exist_ok=True)
+
 
     if separate_spin:
         spin_list = dict_info["spin"]
         spin_list_single = [i[0] for i in spin_list]
         list_spin_unique = np.unique(spin_list_single)
-
+    
     if separate_spin and separate_composition and separate_charges:
         separate_charge_comp_dict = separate_into_charge_and_comp_and_spin(dict_info)
     elif separate_composition and separate_charges:
@@ -637,38 +674,46 @@ def write_dict_to_ase_trajectory(
     else:
         separate_charge_comp_dict = None
 
+    breakdown_dict = {}
     # create a folder for each charge
     frame_count_global = 0
 
     if separate_charges and separate_spin:
         for charge in list_charge_unique:
-            frame_count_charge = 0
-            if not os.path.exists(
-                root_out + "/charge_" + str(charge)
-            ) and not os.path.exists(root_out + "/charge_" + "neg_" + str(charge)):
-                charge_temp = int(charge)
-                if charge < 0:
-                    charge_temp = "neg_" + str(abs(charge_temp))
-                os.makedirs(root_out + "/charge_" + str(charge_temp), exist_ok=True)
-
-            for spin in list_spin_unique:
+            for folder in list_split_folders:
+                frame_count_charge = 0
                 if not os.path.exists(
-                    root_out + "/charge_" + str(charge_temp) + "/spin_" + str(int(spin))
-                ):
-                    os.makedirs(
-                        root_out
-                        + "/charge_"
-                        + str(charge_temp)
-                        + "/spin_"
-                        + str(int(spin)),
-                        exist_ok=True,
-                    )
+                    root_out + folder + "/charge_" + str(charge)
+                ) and not os.path.exists(root_out + "/charge_" + "neg_" + str(charge)):
+                    charge_temp = int(charge)
+                    if charge < 0:
+                        charge_temp = "neg_" + str(abs(charge_temp))
+                    os.makedirs(root_out + folder + "/charge_" + str(charge_temp), exist_ok=True)
+
+                for spin in list_spin_unique:
+                    if not os.path.exists(
+                        root_out + folder + "/charge_" + str(charge_temp) + "/spin_" + str(int(spin))
+                    ):
+                        os.makedirs(
+                            root_out
+                            + folder
+                            + "/charge_"
+                            + str(charge_temp)
+                            + "/spin_"
+                            + str(int(spin)),
+                            exist_ok=True,
+                        )
+                    if charge not in breakdown_dict:
+                        breakdown_dict[charge] = {} 
+                    breakdown_dict[charge][spin] = {"train": 0, "val": 0, "test": 0}
 
             for spin_key, dict_info_temp in separate_charge_comp_dict[charge].items():
                 for comp_key, dict_info in dict_info_temp.items():
+                    folder_selected = draw_split(train_prop, val_prop, test_prop)
                     comp_count = 0
                     file = (
                         root_out
+                        + folder_selected
                         + "/charge_"
                         + str(charge_temp)
                         + "/spin_"
@@ -702,29 +747,39 @@ def write_dict_to_ase_trajectory(
                                 comp_count += 1
                                 frame_count_charge += 1
                                 frame_count_global += 1
-
-                    print(
-                        "frames to {}: \t\t {}".format(
-                            file.split("/")[-1].split(".")[0], comp_count
-                        )
-                    )
-            print(
-                "frames charge {} folder:\t {}".format(charge_temp, frame_count_charge)
-            )
+                                breakdown_dict[charge][spin_key][folder_selected] += 1
+                    
+                    #print(
+                    #    "frames to {}: \t\t {}".format(
+                    #        file.split("/")[-1].split(".")[0], comp_count
+                    #    )
+                    #)
+                #print(
+                #    "frames charge {} folder:\t {}".format(charge_temp, frame_count_charge)
+                #)
+            # print breakdown pretty 
+        for charge in breakdown_dict:
+            for spin in breakdown_dict[charge]:
+                print("charge: {} spin: {} counts: {}".format(charge, spin, breakdown_dict[charge][spin]))                
         print("frames total: \t\t {}".format(frame_count_global))
 
     elif separate_charges:
         for charge in list_charge_unique:
-            frame_count_charge = 0
-            if not os.path.exists(root_out + str(charge)):
-                charge_temp = int(charge)
-                if charge < 0:
-                    charge_temp = "neg_" + str(abs(charge_temp))
-                os.makedirs(root_out + str(charge_temp), exist_ok=True)
+            for folder in list_split_folders:
+                frame_count_charge = 0
+                if not os.path.exists(root_out + folder + str(charge)):
+                    charge_temp = int(charge)
+                    if charge < 0:
+                        charge_temp = "neg_" + str(abs(charge_temp))
+                    os.makedirs(root_out + folder + str(charge_temp), exist_ok=True)
+                breakdown_dict[charge] = {"train": 0, "val": 0, "test": 0}
+
 
             for comp_key, dict_info in separate_charge_comp_dict[charge].items():
                 comp_count = 0
-                file = root_out + str(charge_temp) + "/{}.xyz".format(comp_key)
+                folder_selected = draw_split(train_prop, val_prop, test_prop)
+                file = root_out + folder_selected + str(charge_temp) + "/{}.xyz".format(comp_key)
+
                 with open(file, "w") as f:
                     for ind_frame, (
                         energies_frame,
@@ -752,24 +807,19 @@ def write_dict_to_ase_trajectory(
                             comp_count += 1
                             frame_count_charge += 1
                             frame_count_global += 1
+                            breakdown_dict[charge][folder_selected] += 1
 
-                print(
-                    "frames to {}: \t\t {}".format(
-                        file.split("/")[-1].split(".")[0], comp_count
-                    )
-                )
-
-            print(
-                "frames charge {} folder:\t {}".format(charge_temp, frame_count_charge)
-            )
-        print("frames total: \t\t {}".format(frame_count_global))
+            print(breakdown_dict)
+            print("frames total: \t\t {}".format(frame_count_global))
 
     else:
         print("writing as a single file")
-        if not os.path.exists(root_out):
-            # make it if it doesn't
-            os.makedirs(root_out, exist_ok=True)
-
+        for folder in list_split_folders:
+            if not os.path.exists(root_out + folder):
+                # make it if it doesn't
+                os.makedirs(root_out + folder, exist_ok=True)
+            #folder_selected = draw_split(train_prop, val_prop, test_prop)
+        
         with open(root_out + "combined.xyz", "w") as f:
             for ind_frame, (
                 energies_frame,
@@ -803,4 +853,4 @@ def write_dict_to_ase_trajectory(
                                 grad[2],
                             )
                         )
-        print("Wrote {} frames to {}".format(frame_count_global, root_out))
+            print("Wrote {} frames to {}".format(frame_count_global, root_out))
